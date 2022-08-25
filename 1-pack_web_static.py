bbb6523306cf316dd8e2e1e260ed003676f81219
LesLor <leslie.paz.am@gmail.com>
2022-08-25 22:03:33 +0000

    Changing the position of the import moduls

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive"""
-from datetime import datetime
-from fabric.api import local
-from os.path import exists
 
 
 def do_pack():
@@ -10,6 +7,9 @@ def do_pack():
     from the contents of the web_static folder
     of your AirBnB Clone repo,
     using the function do_pack. """
+    from datetime import datetime
+    from fabric.api import local
+    from os.path import exists
     today = datetime.now().strftime("%Y%m%d%H%M%S")
     file_name = f'web_static_{today}.tgz'
 
