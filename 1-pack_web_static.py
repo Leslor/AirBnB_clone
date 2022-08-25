b8fd81e9460ec8c3ce56eb690e5cf5ef71cada77
LesLor <leslie.paz.am@gmail.com>
2022-08-25 21:23:11 +0000

    Feat: task1 and requirements file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive"""
+from datetime import datetime
+from fabric.api import local
+from os.path import exists
+
+
+def do_pack():
+    """Fabric script that generates a .tgz archive
+    from the contents of the web_static folder
+    of your AirBnB Clone repo,
+    using the function do_pack. """
+    today = datetime.now()
+    dt_string = today.strftime("%d/%m/%Y %H:%M:%S")
+    file_name = f'web_static_{dt_string}.tgz'
+
+    folder_version = exists("versions")
+    if folder_version is False:
+        local('mkdir versions')
+    local("tar -cvzf versions/{}".format(file_name))
+    return file_name
