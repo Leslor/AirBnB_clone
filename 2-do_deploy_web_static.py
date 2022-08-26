67ae147f7805e71e78e5fb0f405e7fd1af0f89a3
LesLor <leslie.paz.am@gmail.com>
2022-08-26 04:44:19 +0000

    Feat: Task-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from os.path import exists
 from fabric.api import *
 env.hosts = ['3.80.49.204', '34.229.127.31']
 
+
 def do_pack():
     """Fabric script that generates a .tgz archive
     from the contents of the web_static folder
@@ -23,26 +24,29 @@ def do_pack():
     except Exception:
         return None
 
+
 def do_deploy(archive_path):
+    """ Fabric script that  distributes
+    an archive to your web servers, using
+    the function do_deploy"""
     if exist(archive_path):
         file_name = archive_path[9:]
-        name  = file_name[:-4]
-        #Upload the archive to the /tmp/ directory of the webserver
+        name = file_name[:-4]
+        # Upload the archive to the /tmp/ directory of the webserver
         directory = '/tmp/{}'.format(file_name)
-        #directory = /tmp/web_static_89808988.tgz
+        # directory = /tmp/web_static_89808988.tgz
         put(archive_path, directory)
         f_web = "/data/web_static/releases/{}/".format(name)
-        #Uncompress the archive to the folder /data/web_static/
+        # Uncompress the archive to the folder /data/web_static/
         run('mkdir -p {}'.format(f_web))
-        #/data/web_static/releases/web_static_098098988
+        # /data/web_static/releases/web_static_098098988
         run('tar -xf {} -C {}'.format(directory, f_web))
-        #Delete the archive from the web server
-        #Delete /tmp/web_static_898980.tgz
+        # Delete the archive from the web server
+        # Delete /tmp/web_static_898980.tgz
         run('rm /tmp/{}'.format(file_name))
         run("mv -f {}web_static/* {}".format(f_web, f_web))
         run("rm -rf {}web_static".format(f_web))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(f_web))
         return True
-
-     return False
+    return False
