55df08d1f41fc0d77eecab7c25aa71cc47fbf55b
LesLor <leslie.paz.am@gmail.com>
2022-08-29 03:40:49 +0000

    Fix: task2-v3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,24 +7,6 @@ from fabric.api import put, run, env
 env.hosts = ['3.80.49.204', '34.229.127.31']
 
 
-def do_pack():
-    """Fabric script that generates a .tgz archive
-    from the contents of the web_static folder
-    of your AirBnB Clone repo,
-    using the function do_pack. """
-    today = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = 'versions/web_static_{}.tgz'.format(today)
-
-    folder_version = exists("versions")
-    if folder_version is False:
-        local('mkdir -p versions')
-    try:
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception:
-        return None
-
-
 def do_deploy(archive_path):
     """ Fabric script that  distributes
     an archive to your web servers, using
