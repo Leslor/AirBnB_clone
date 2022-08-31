fdd93b7f338d619d5fc476e5bf44af42477b7552
LesLor <leslie.paz.am@gmail.com>
2022-08-31 15:15:22 +0000

    Fix: version 2 - try Except

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,7 +12,10 @@ def do_deploy(archive_path):
     an archive to your web servers, using
     the function do_deploy"""
     # archive_path = versions/web_static_220920223.tgz
-    if exists(archive_path):
+
+    if not exists(archive_path):
+        return False
+    try:
         file_name = archive_path[9:]
         # file_name = web_static_220920223.tgz
         name = file_name[:-4]
@@ -23,16 +26,14 @@ def do_deploy(archive_path):
         # Uncompress to data/web_static/releases/<archive>
         f_web = "/data/web_static/releases/"
         # Uncompress the archive to the folder /data/web_static/
-        # run('mkdir -p {}'.format(f_web))
         run('mv {} {}'.format(directory, f_web))
         # /data/web_static/releases/web_static_098098988
         run('tar -xf {}{}'.format(f_web, name))
         # Delete the archive from the web server
-        # Delete /tmp/web_static_898980.tgz
         run('rm {}{}'.format(f_web, file_name))
         # Delete simbolic Link
-        # run("rm -rf {}web_static".format(f_web))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}name /data/web_static/current".format(f_web))
+        run("ln -s {}{} /data/web_static/current".format(f_web, name))
         return True
-    return False
+    except Exception:
+        return False
