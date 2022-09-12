9ad7ce9b9f9ed40c1a0807c771e056ae956d1f75
Leslor <leslie.paz.am@gmail.com>
2022-09-11 20:47:05 -0500

    Fix: PEP8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def states_list():
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exc):
     """Remove the current SQLAlchemy session."""
